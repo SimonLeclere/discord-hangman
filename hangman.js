b58927e306e31f818e170de55b62d3c515a78731
LACOSTAR91 <66567863+LACOSTAR91@users.noreply.github.com>
2022-03-01 15:05:26 +0100

    Update the file for V13
    - Update the file for accepting using interaction on V13.6.0
    - Add lang system

--- hangman.js
+++ hangman.js
@@ -1,56 +1,39 @@
-const Discord = require('discord.js');
+const { Embed } = require('@discordjs/builders');
 const randomWord = require('random-word');
+const randomWordFR = require('random-word-fr');
 
 class hangman {
-    constructor(word = null, channel, players, messages, displayWordOnGameOver) {
-        this.word = word || randomWord();
+    constructor(word = null, interaction, players, messages, displayWordOnGameOver, lang) {
+        this.lang = lang || 'en';
+        this.word = word || this.lang === 'en' ? randomWord() : randomWordFR();
         this.lives = 6;
         this.progress = hangman.hyphenString(this.word.length);
         this.remaining = this.word.length;
         this.misses = [];
         this.status = 'in progress';
         this.gameOver = false;
-        this.channel = channel;
+        this.interaction = interaction;
         this.message = null;
         this.players = players;
         this.messages = messages;
         this.displayWordOnGameOver = displayWordOnGameOver;
     }
 
-    static hyphenString(n) {
-        return '-'.repeat(n);
-    }
-
-    replaceChar(char) {
-        for (let i = 0; i < this.word.length; ++i) {
-            if (this.word[i] === char) {
-                this.progress = this.progress.substring(0, i) + this.word[i] + this.progress.substring(i + this.word[i].length);
-                this.remaining--;
-            }
-        }
-    }
+    static hyphenString(n) { return '-'.repeat(n); };
+    replaceChar(char) { for (let i = 0; i < this.word.length; ++i) { if (this.word[i] === char) { this.progress = this.progress.substring(0, i) + this.word[i] + this.progress.substring(i + this.word[i].length); this.remaining--; }; }; };
 
     async showProgress() {
-        const embed = new Discord.MessageEmbed()
-            .setDescription('```\n' + this.getFigure() + '```')
-            .addField('Players', this.playerlist())
-            .setColor(this.gameOver ? (this.status === 'won' ? '#00CC00' : '#E50000') : '');
-
-        if (this.message) await this.message.edit({
-            embed: embed
-        });
-        else this.message = await this.channel.send({
-            embed: embed
-        });
-    }
+        const embed = new Embed().setDescription('```\n' + this.getFigure() + '```').addField({ name: 'Players', value: this.playerlist() }).setColor(this.gameOver ? (this.status === 'won' ? 0x00CC00 : 0xE50000) : 0x000000);
+        if (this.message) await this.message.edit({ embeds: [embed] });
+        else this.message = await this.interaction.channel.send({ embeds: [embed] });
+    };
     
     playerlist() {
-        if (!this.players.length) return this.messages.noplayersleft
-        const filter = this.players.slice(0, 3)
-        const remaining = this.players.length - filter.length === 0 ? '' : `+ ${this.players.length - filter.length} more`
-
-        return filter.join('\n') + remaining
-    }
+        if (!this.players.length) return this.messages.noplayersleft;
+        const filter = this.players.slice(0, 3);
+        const remaining = this.players.length - filter.length === 0 ? '' : `+ ${this.players.length - filter.length} more`;
+        return filter.join('\n') + remaining;
+    };
 
     getFigure() {
         return `
@@ -62,68 +45,40 @@ class hangman {
          |
      =========  ${this.gameOver ? (this.status === 'won' ? this.messages.won : this.messages.gameOver) : ''} ${this.displayWordOnGameOver && this.gameOver && this.status !== 'won' ? this.messages.gameOverMsg.replace(/{word}/gi, this.word) : ''}
         `;
-    }
+    };
 
     guess(c) {
-        if (this.progress.includes(c)) {
-            this.lives--;
-        } else if (this.word.includes(c)) {
-            this.replaceChar(c);
-        } else {
-            if (!this.misses.includes(c)) this.misses.push(c);
-            this.lives--;
-        }
-
+        if (this.progress.includes(c)) this.lives--; 
+        else if (this.word.includes(c)) this.replaceChar(c);
+        else { if (!this.misses.includes(c)) this.misses.push(c); this.lives--; }
         if (this.lives === 0) this.status = 'lost';
         else if (this.remaining === 0) this.status = 'won';
-        return {
-            status: this.status,
-            progress: this.progress,
-            misses: this.misses,
-            lifes: this.lives
-        };
-    }
+        return { status: this.status, progress: this.progress, misses: this.misses, lifes: this.lives };
+    };
 
     guessAll(word) {
-        if (this.word === word) {
-            this.progress = this.word;
-            this.status = 'won';
-        } else {
-            this.lives--;
-        }
+        if (this.word === word) { this.progress = this.word; this.status = 'won'; } 
+        else this.lives--; 
         return this.status === 'won';
-    }
+    };
 
     async start() {
         await this.showProgress();
-
         const filter = ((m) => this.players.find((p) => (p.id == m.author.id)));
-        const collector = this.channel.createMessageCollector(filter, {
-            time: 900000
-        });
+        const collector = this.interaction.channel.createMessageCollector(filter, { time: 900_000 });
 
         return new Promise(resolve => {
             collector.on('collect', async (m) => {
-
                 if (!m.content.match(new RegExp(`^[A-Za-zÀ-ú](?:.{0}|.{${this.word.length - 1}})$`))) return;
-
                 const c = m.content.toLowerCase();
                 m.delete();
-                if (m.content.length === this.word.length) {
-                    if (this.guessAll(c) === false) this.players = this.players.filter(p => p.id !== m.author.id);
-                } else if (m.content.length === 1) {
-                    this.guess(c);
-                } else {
-                    return;
-                }
+                if (m.content.length === this.word.length) { if (this.guessAll(c) === false) this.players = this.players.filter(p => p.id !== m.author.id); } 
+                else if (m.content.length === 1) this.guess(c); 
+                else return; 
                 await this.showProgress();
 
-                if (this.status !== 'in progress') {
-                    collector.stop();
-                } else if (this.players.length < 1) {
-                    collector.stop();
-                    this.status = 'lost';
-                }
+                if (this.status !== 'in progress') collector.stop(); 
+                else if (this.players.length < 1) { collector.stop(); this.status = 'lost'; }
 
             });
             collector.on('end', async () => {
@@ -132,9 +87,7 @@ class hangman {
                 resolve();
             });
         });
-    }
-
-
-}
+    };
+};
 
 module.exports = hangman;
